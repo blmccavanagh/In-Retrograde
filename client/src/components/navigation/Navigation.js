3ccc5a9480282a582f376518589944e5980d701f
Bridget Louise McCavanagh <blmccavanagh@gmail.com>
2021-08-10 21:55:15 +0800

    Routing between landing, sign up and login pages added.

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Landing from '../../pages/landing/Landing';
 
 export default function Navigation() {
     return (
         <div className="pMono inline-block text-right">
-            <h3 className="text-white ml-96 font-bold text-2xl">
-                Logout
-                <Link key="Logout" to={ Landing } ></Link>
-            </h3>
+            <Link key="Logout" to="/">
+                <button className="text-white ml-96 font-bold text-2xl">
+                    Logout
+                </button>
+            </Link>
         </div>
     )
 }
